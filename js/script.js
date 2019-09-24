fc486af31b271fb03f85139b39263d5540adce51
Kardanov Nikolai <53314664+Nikolai2104@users.noreply.github.com>
2019-09-24 14:24:05 +0700

    Add blog slider

--- js/script.js
+++ js/script.js
@@ -3,3 +3,65 @@ $('.slider').slick({
   infinite: true,
   speed: 500,
 });
+$('#blog').slick({
+  centerMode: true,
+  centerPadding: '80px',
+  slidesToShow: 4,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '80px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '150px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '100px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 520,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '20px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
